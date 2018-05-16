946aadf6060290152fc412222aaa91efa74230da
Dennis Kok <dennismonnetje@gmail.com>
2018-05-16 13:09:18 +0200

    Implemented cached thread pool

--- src/com/m2e4/Main.java
+++ src/com/m2e4/Main.java
@@ -3,7 +3,20 @@ package com.m2e4;
 import com.m2e4.DataBase.DataBase;
 import com.m2e4.gui.MainFrame;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class Main {
+
+    private static ExecutorService threadPool = Executors.newCachedThreadPool(new ThreadFactory() {
+        private int counter = 0;
+
+        @Override
+        public Thread newThread(Runnable runnable) {
+            return new Thread(runnable, String.format("KBSPool-Thread-%d", counter++));
+        }
+    });
 
     public static void main(String[] args) {
         DataBase dbc = new DataBase();
@@ -11,6 +24,7 @@ public class Main {
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
                 dbc.closeConn();
+                threadPool.shutdownNow();
                 System.exit(0);
             }
         });
@@ -21,5 +35,9 @@ public class Main {
         catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public static ExecutorService getThreadPool() {
+        return threadPool;
     }
 }
